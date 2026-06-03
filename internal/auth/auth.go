62c2cf9f20fb3eebe826627f84f8a020bb3608a4
KayOI <kay@onlineimage.com>
2026-06-03 10:01:52 -0600

    added MakeJWT method

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -19,4 +21,23 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	userIDString := userID.String()
+
+	issTime := time.Now().UTC()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:	"chirpy-access",
+		IssuedAt: jwt.NewNumericDate(issTime),
+		ExpiresAt: jwt.NewNumericDate(issTime.Add(expiresIn)),
+		Subject: userIDString,
+	})
+	secretBytes := []byte(tokenSecret)
+	signedToken, err := token.SignedString(secretBytes)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
 }
